84755392415f0c699b9a2520048a9b435963bdb7
cymbiotica <terry.esposito@gmail.com>
2018-06-24 16:55:46 -0400

    everything is broken

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -4,14 +4,13 @@ const events = require('../scripts/auth/authEvents')
 const changeView = require('../scripts/templates/helpers/getTemplate')
 
 $(() => {
-  
   // auth events+
   $('#sign-up').on('submit', events.onSignup)
   $('#sign-in').on('submit', events.onSignIn)
   $('#change-password').on('sumbit', events.onChangePassword)
   $('#sign-out').on('submit', events.onSignOut)
-  $('#game').on('click', changeView.getTemplate())
-  changeView.getTemplate()
+  // $('#game').on('click', changeView.getTemplate())
+  // changeView.getTemplate()
 })
 
 // $('#main-content').ready(changeView.getTemplate())
