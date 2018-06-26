1370f84c59a278efddbf7303c9de16d2ccb95271
cymbiotica <terry.esposito@gmail.com>
2018-06-26 15:12:48 -0400

    starting on game API.

--- assets/scripts/auth/index.js
+++ assets/scripts/auth/index.js
@@ -4,6 +4,7 @@ const events = require('./authEvents')
 $(() => {
   $('#sign-up').on('submit', events.onSignup)
   $('#sign-in').on('submit', events.onSignIn)
-  $('#change-password').on('sumbit', events.onChangePassword)
+  // $('#change-password').on('submit', () => console.log('change password submitted'))
+  $('#change-password').on('submit', events.onChangePassword)
   $('#sign-out').on('submit', events.onSignOut)
 })
--- assets/scripts/utility/gameConfig.js
+++ assets/scripts/utility/gameConfig.js
@@ -1,17 +1,1 @@
 'use strict'
-
-const numberOfPlayers = 2
-const currentPlayer = 0
-const move = 0
-const points1 = 0 // player 1 points
-const points2 = 0 // player 2 points
-const size = 3
-
-module.exports = {
-  numberOfPlayers,
-  currentPlayer,
-  move,
-  points1,
-  points2,
-  size
-}
