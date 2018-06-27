b5124a8986796c514ec30429ac7156539ed8e36c
cymbiotica <terry.esposito@gmail.com>
2018-06-27 18:27:45 -0400

    preparing to load up togithub pages

--- assets/scripts/auth/temp.js
+++ assets/scripts/auth/temp.js
@@ -1,17 +1,9 @@
 const gameApi = require('../game/gameApi')
-const game = require('../game/xsie-osies')
-let gameStore = require('../utility/gameStore')
 const gameUI = require('../game/gameUI')
-
-// console.log('sd')
 
 const onCreateGame = function (user) {
   gameApi.createGame(user)
     .then(gameUI.onSigninSuccessCreateGame)
-    // .then(function (data) {
-    //   gameStore = data
-    //   game.drawBoard()
-    // })
     .catch(() => console.log('Login needed.')) // output login required
 }
 
